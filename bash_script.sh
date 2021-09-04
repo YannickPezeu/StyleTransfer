35a23be4c1cf0c924d520b86b42ea6dbaa21f876
Yannick Pezeu <53047800+YannickPezeu@users.noreply.github.com>
2021-09-04 16:06:31 +0200

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+# This script performs the transformation of all the files in "origins/" with the style Monet1-.jpg
 
 FILES="origins/*"
 for f in $FILES
 do
-    python main.py $f
+    python main.py Monet1-.jpg $f 
     sleep 1
 done
 
